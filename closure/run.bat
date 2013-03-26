dd9a73316bb9e8d2dc07249a94897c307036a513
Robert Carnecky <crobi@inf.ethz.ch>
2013-03-26 17:25:59 +0100

    Improve the closure build script

--- closure/run.bat
+++ closure/run.bat
@@ -1,2 +1,5 @@
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --formatting PRETTY_PRINT --warning_level VERBOSE --summary_detail_level 3 --js ../ColladaLoader2.js --externs threejs-exports.js --externs common-exports.js --js_output_file ColladLoader2Closure.js
-pause
+@echo Building ColladaLoader2.js
+@call node ..\node_modules\coffee-script\bin\coffee -c ..\ColladaLoader2.coffee
+@echo Building ColladaLoader2-closure.js
+@call java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --formatting PRETTY_PRINT --warning_level VERBOSE --summary_detail_level 3 --js ../ColladaLoader2.js --externs threejs-exports.js --externs common-exports.js --js_output_file ColladLoader2-closure.js
+@pause
