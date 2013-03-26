02c4aad00cf99ef0b83f601c68bf14ee5d0e54fc
Robert Carnecky <crobi@inf.ethz.ch>
2013-03-26 18:20:08 +0100

    Add a comment in the makefile for how to use the new coffeescript compiler

--- makeColladaLoader2.bat
+++ makeColladaLoader2.bat
@@ -1,3 +1,4 @@
 @CALL node node_modules\coffee-script\bin\coffee -c ColladaLoader2.coffee
+@REM @CALL node node_modules\coffee-script-redux\bin\coffee -j -i ColladaLoader2.coffee ColladaLoader2.js
 @CALL node node_modules\uglify-js\bin\uglifyjs ColladaLoader2.js > ColladaLoader2-min.js
 @pause
