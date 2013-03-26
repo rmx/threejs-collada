7b7487a5e00f17d72370c0607f11d253c09c3d3e
Robert Carnecky <crobi@inf.ethz.ch>
2013-03-26 17:04:13 +0100

    Update to coffeescript 1.6.2

--- makeColladaLoader2.bat
+++ makeColladaLoader2.bat
@@ -1,3 +1,3 @@
-@CALL node node_modules\coffee-script\bin\coffee -c ColladaLoader2
+@CALL node node_modules\coffee-script\bin\coffee -c ColladaLoader2.coffee
 @CALL node node_modules\uglify-js\bin\uglifyjs ColladaLoader2.js > ColladaLoader2-min.js
 @pause
