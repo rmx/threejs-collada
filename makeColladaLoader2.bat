d71a6563c3774900fb6869d3a671587c53d09863
Robert Carnecky <crobi@inf.ethz.ch>
2013-03-27 12:24:18 +0100

    Add the coffeescript redux compiler

--- makeColladaLoader2.bat
+++ makeColladaLoader2.bat
@@ -1,4 +1,8 @@
+@ECHO building ColladaLoader2.js
 @CALL node node_modules\coffee-script\bin\coffee -c ColladaLoader2.coffee
-@REM @CALL node node_modules\coffee-script-redux\bin\coffee -j -i ColladaLoader2.coffee ColladaLoader2.js
+@ECHO building ColladaLoader2-redux.js
+@CALL node node_modules\coffee-script-redux\bin\coffee -j -i ColladaLoader2.coffee -o ColladaLoader2-redux.js
+@ECHO building ColladaLoader2-min.js
 @CALL node node_modules\uglify-js\bin\uglifyjs ColladaLoader2.js > ColladaLoader2-min.js
+
 @pause
