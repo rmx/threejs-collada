e79f58abc810f5896a4d6881589b6093d8fdc7db
Robert Carnecky <robin@dbservice.com>
2013-04-08 16:44:46 +0200

    Disable the coffescript 2 compiler (not stable yet, does not support full coffeescript feature set)

--- makeColladaLoader2.bat
+++ makeColladaLoader2.bat
@@ -1,7 +1,7 @@
 @ECHO building ColladaLoader2.js
 @CALL node node_modules\coffee-script\bin\coffee -c ColladaLoader2.coffee
-@ECHO building ColladaLoader2-redux.js
-@CALL node node_modules\coffee-script-redux\bin\coffee -j -i ColladaLoader2.coffee -o ColladaLoader2-redux.js
+@REM @ECHO building ColladaLoader2-redux.js
+@REM @CALL node node_modules\coffee-script-redux\bin\coffee -j -i ColladaLoader2.coffee -o ColladaLoader2-redux.js
 @ECHO building ColladaLoader2-min.js
 @CALL node node_modules\uglify-js\bin\uglifyjs ColladaLoader2.js > ColladaLoader2-min.js
 
